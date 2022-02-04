c86d9e21a5b989c7371dd1fdfa974981c05f7e21
sangeetha <Artemis6161@github.com>
2022-02-04 20:48:35 +0530

    final

--- src/componenet/tictactoe.js
+++ src/componenet/tictactoe.js
@@ -1,13 +1,13 @@
 import React,{ useEffect, useState } from 'react'
 import "./tic.css"
 import Pattern from "./pattern"
-
+const init = ['','','','','','','','',''];
  function Tictactoe() {
-    const [board, setBoard] = useState(['','','','','','','','',''])
+    const [board, setBoard] = useState([...init])
     const [player, setPlayer] = useState("x")
     const[lIdx,setLidx] = useState(-1);
     const changeTile = (idx) =>{
-      if (board[idx] !=='') return;
+      if (board[idx] !=="") return;
       setLidx(idx);
     setBoard((board) =>{
         return board.map((val,i) =>{
@@ -20,16 +20,26 @@ import Pattern from "./pattern"
 };
 const checkWin = () => {
   if(lIdx < 0) return;
+  // console.log(player);
   const checkArr = Pattern[lIdx];
   const prevPlayer = player === "x" ? "o":"x";
   checkArr.forEach((arr) =>{
-    if(board[arr[0]]=== prevPlayer && board[arr[1]] && board[arr[2]])
-    {alert(`${prevPlayer} won the game`);
+    if(
+      board[arr[0]]=== prevPlayer && 
+      board[arr[1]]=== prevPlayer && 
+      board[arr[2]]=== prevPlayer)
+      {
+    alert(`${prevPlayer} won the game`);
+    reset();
   }
 })
 }
  
- 
+ const reset = () =>{
+  setBoard([...init])
+  setPlayer("x");
+  setLidx(-1);
+ }
 
 useEffect(() =>{
   checkWin();
@@ -44,8 +54,12 @@ useEffect(() =>{
         )
         })}
       
-            
+     
       </div>
+      
+      <p style={{textAlign: 'center'}}>
+      <button onClick={reset}>Reset</button> 
+      </p>
       </>
   )}
 
